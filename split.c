0afc02a2ca3f4732200b0d6173eb5709bde475b2
Logosh <101455421+Niglo-42@users.noreply.github.com>
2026-07-26 21:06:55 +0200

    Refactor t_split function and improve handling

--- split.c
+++ split.c
@@ -11,18 +11,19 @@ int ft_strlen(char *s)
     return (len);
 }
 
-char    **t_split(char **buf, char *s, int acc, int nw)
+char    **t_split(char *s, int acc, int nw)
 {
     int     nb_char;
     int     i;
+    char    **buf;
 
     nb_char = 0;
+    i = -1;
     if (*s == 0)
     {
         buf = calloc((nw + 1) * sizeof(char *) + acc + nw, 1);
-        i = 0;
-        while (i < nw)
-            buf[i++] = (char *)((char *)buf + (nw + 1) * sizeof(char *));
+        while (++i < nw)
+            buf[i] = (char *)((char *)buf + (nw + 1) * sizeof(char *));
         return (buf);
     }
     while (*s && ((*s >= 8 && *s <= 13) || *s == 32))
@@ -30,10 +31,9 @@ char    **t_split(char **buf, char *s, int acc, int nw)
     while (s[nb_char] && !((
         s[nb_char] >= 8 && s[nb_char] <= 13) || s[nb_char] == 32))
         nb_char++;
-    buf = t_split(buf, s + nb_char, acc + nb_char, nw + (nb_char > 0));
+    buf = t_split(s + nb_char, acc + nb_char, nw + (nb_char > 0));
     if (nb_char == 0)
-        return buf;
-    i = -1;
+        return (buf);
     buf[nw] = buf[nw] + acc + nw;
     while (++i < nb_char)
         buf[nw][i] = s[i];
@@ -48,10 +48,13 @@ int main(int ac, char **av)
     int len;
     int i;
     int j;
-    //char s[] = "abricot_boite   3\nbacking_powder  200\nblanc_oeuf      9000\nbeurre          1500\nbeurre_cacao    0\ncafe_arome      20\ncafe_grain      500\ncassonnade      1000\nchoc_blanc      5000\nchoc_noir       4000\ncitron          2000\ncompote_pomme	4000\ncreme			5000\nfarine			50000\n";
-char s[] = "\tA\nBB\rCCC\fDDDD\vEEEEE FFFFFF";
-i = 0;
-    buf = t_split(buf, s, 0, 0);
+    //char s[] = "abricot_boite   3\nbacking_powder  200\nblanc_oeuf      9000\nbeurre          1500\nbeurre_cacao    0\ncafe_arome      20\ncafe_grain      500\ncassonnade      1000\nchoc_blanc      5000\nchoc_noir       4000\ncitron          2000\ncompote_pomme     4000\ncreme                     5000\nfarine                        50000\n";
+    char s[100] = "\tA\nBB\rCCC\fDDDD\vEEEEE FFFFFF";
+    i = 0;
+    if (ac == 2)
+        buf = t_split(av[1], 0, 0);
+    else
+        buf = t_split(s, 0, 0);
     while (buf[i] != NULL)
     {
         char *word = buf[i];
